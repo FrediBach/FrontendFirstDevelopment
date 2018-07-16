353777d75c05c029031efca58d8723a5e1123611
Fredi Bach <fredi.bach@unic.com>
2018-07-16 15:49:15 +0200

    Add blockquotes

--- dist/manifest.702a00b3.js
+++ dist/manifest.702a00b3.js
@@ -167,7 +167,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '53904' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '50639' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
